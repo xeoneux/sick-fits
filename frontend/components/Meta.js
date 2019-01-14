f425499598de11624af4c04a2367c36b96d1bfc1
Aayush Kapoor <xeoneux@icloud.com>
2019-01-14 07:10:13 +0530

    Create meta component to set meta info

---
+++ frontend/components/Meta.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link rel="stylesheet" href="/static/nprogress.css" type="text/css" />
+
+    <title>Sick Fits!</title>
+  </Head>
+);
+
+export default Meta;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import Meta from './Meta';
 import Header from './Header';
 
 class Page extends Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         {this.props.children}
       </div>
