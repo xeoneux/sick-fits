f5515a29926093280abdc15612a888768b170958
Aayush Kapoor <xeoneux@icloud.com>
2019-01-14 06:51:36 +0530

    Insert simple header component above pages

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Header from './Header';
+
 class Page extends Component {
   render() {
     return (
       <div>
-        <p>Hey I'm the Page component</p>
+        <Header />
         {this.props.children}
       </div>
     );
