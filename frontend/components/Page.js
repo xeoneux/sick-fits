7446a275d8a310887e72ac4e01c1d780b7ef2ade
Aayush Kapoor <xeoneux@icloud.com>
2019-01-15 08:59:34 +0530

    Add defaults for page theme

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -4,6 +4,16 @@ import styled from 'styled-components';
 import Meta from './Meta';
 import Header from './Header';
 
+const theme = {
+  red: '#FF0000',
+  grey: '#3A3A3A',
+  black: '#393939',
+  maxWidth: '1000px',
+  offWhite: '#EDEDED',
+  lightgrey: '#E1E1E1',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
 const StyledPage = styled.div`
   color: black;
   background-color: white;
