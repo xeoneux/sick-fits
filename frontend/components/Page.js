ed36ab4019ed1e688fb5fc3d54cae0874c557291
Aayush Kapoor <xeoneux@icloud.com>
2019-01-15 08:55:07 +0530

    Create a styled page wrapper

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Meta from './Meta';
 import Header from './Header';
 
+const StyledPage = styled.div`
+  color: black;
+  background-color: white;
+`;
+
+const Inner = styled.div`
+  padding: 2rem;
+  margin: 0 auto;
+  max-width: 1000px;
+`;
+
 class Page extends React.Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
         {this.props.children}
-      </div>
+      </StyledPage>
     );
   }
 }
