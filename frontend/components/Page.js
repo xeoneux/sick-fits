b89ea1e6829580a1f0017992a92c302562fb963c
Aayush Kapoor <xeoneux@icloud.com>
2019-01-15 08:48:04 +0530

    Remove demo style code and use components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Meta from './Meta';
 import Header from './Header';
-
-const MyButton = styled.button`
-  font-size: 50px;
-  background: red;
-
-  span {
-    font-size: ${({ huge }) => (huge ? 100 : 50)}px;
-  }
-`;
 
 class Page extends React.Component {
   render() {
@@ -19,10 +9,6 @@ class Page extends React.Component {
       <div>
         <Meta />
         <Header />
-        <MyButton huge>
-          Click Me
-          <span>💩</span>
-        </MyButton>
         {this.props.children}
       </div>
     );
