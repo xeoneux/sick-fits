6efea9aa075dd703a54069c934d06ac85ad63111
Aayush Kapoor <xeoneux@icloud.com>
2019-01-15 08:36:43 +0530

    Try out styled components for styling button

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
 import Meta from './Meta';
 import Header from './Header';
 
-class Page extends Component {
+const MyButton = styled.button`
+  background: red;
+  font-size: 100px;
+`;
+
+class Page extends React.Component {
   render() {
     return (
       <div>
         <Meta />
         <Header />
+        <MyButton>Click Me</MyButton>
         {this.props.children}
       </div>
     );
