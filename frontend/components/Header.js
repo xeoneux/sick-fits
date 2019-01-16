62f90d1ffbf29f44d96c04b7847a72a953b64db9
Aayush Kapoor <xeoneux@icloud.com>
2019-01-16 05:54:10 +0530

    Update logo in header styles

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,15 +1,32 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  z-index: 2;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    padding: 0.5rem 1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+    background: ${({ theme }) => theme.red};
+  }
+`;
 
 const Header = () => (
   <div>
     <div className="bar">
-      <h1>
+      <Logo>
         <Link href="/">
           <a>Sick Fits</a>
         </Link>
-      </h1>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
