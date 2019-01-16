7ff623c6fca6c85fb56b3d3e26986da89be70c38
Aayush Kapoor <xeoneux@icloud.com>
2019-01-16 06:36:18 +0530

    Inject global styles through styled components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import styled, { injectGlobal, ThemeProvider } from 'styled-components';
 
 import Meta from './Meta';
 import Header from './Header';
@@ -13,6 +13,26 @@ const theme = {
   lightgrey: '#E1E1E1',
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
+
+injectGlobal`
+  html {
+    font-size: 10px;
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    line-height: 2;
+    font-size: 1.5rem;
+  }
+
+  *,
+  *:after,
+  *:before {
+    box-sizing: inherit;
+  }
+`;
 
 const StyledPage = styled.div`
   background-color: white;
