8f4abb2649a5548bcaeea532c47937817423db84
Aayush Kapoor <xeoneux@icloud.com>
2019-01-21 11:28:21 +0530

    Write script to start the created server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+const { config } = require('dotenv');
+
+const db = require('./db');
+const createServer = require('./createServer');
+
+config({ path: 'variables.env' });
+
+createServer().server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  options =>
+    console.log(`The server is running on http://localhost:${options.port}`)
+);
