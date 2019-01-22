5da1bca4372a89ff0aa39ada8ddd47d25f9684e2
Aayush Kapoor <xeoneux@icloud.com>
2019-01-23 02:40:04 +0530

    Create mutation resolver for create item

--- backend/src/resolvers/Mutations.js
+++ backend/src/resolvers/Mutations.js
@@ -1,3 +1,7 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    return ctx.db.mutation.createItem({ data: { ...args } }, info);
+  },
+};
 
 module.exports = Mutations;
